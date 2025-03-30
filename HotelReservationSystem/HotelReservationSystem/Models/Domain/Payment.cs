f87424a45a93787dec7b17d8d512098193a82b20
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-03-30 20:42:16 +0200

    Data base models

---
+++ HotelReservationSystem/HotelReservationSystem/Models/Domain/Payment.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelReservationSystem.Models.Domain
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; } = default!;
+        public string Status { get; set; } = default!;
+
+        public int ReservationId { get; set; }
+    }
+}
---
+++ HotelReservationSystem/HotelReservationSystem/Models/Domain/Reservation.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelReservationSystem.Models.Domain
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string Status { get; set; } = default!;
+       
+        public int RoomId { get; set; }
+        public Room Room { get; set; } = default!;
+
+        public int GuestId { get; set; }
+        public Guest Guest { get; set; } = default!;
+
+        public Payment Payment { get; set; } = default!;
+    }
+}
