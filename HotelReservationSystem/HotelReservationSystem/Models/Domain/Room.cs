0604345cf7bf98caf49ec458348f579cc8a409fc
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-06-05 15:14:21 +0200

    image field added to room model and index.cshtml

--- HotelReservationSystem/HotelReservationSystem/Models/Domain/Room.cs
+++ HotelReservationSystem/HotelReservationSystem/Models/Domain/Room.cs
@@ -8,6 +8,8 @@
         public decimal PricePerNight { get; set; }
         public bool IsAvailable { get; set; }
 
+        public string? Image { get; set; }
+
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
     }
 }
