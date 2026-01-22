67a213f9bf3f5678a1f38c9cc99488980587cacb
Mikołaj Kocik <mikolajkocik@onet.pl>
2026-01-22 22:29:07 +0100

    Update header with  animations

--- HotelReservationSystem/HotelReservationSystem/wwwroot/js/site.js
+++ HotelReservationSystem/HotelReservationSystem/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.querySelectorAll(".date").forEach(input => {
+    input.addEventListener('click', function () {
+        if (this.showPicker()) {
+            this.showPicker();
+        }
+    });
+});
